3f6edf392770608c96c645d2e2f337f79928be4e
sud0pacman <mukhammad.kammoliddin@gmail.com>
2025-11-06 17:53:12 +0500

    first commit

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "NixOS configuration";
+ 
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    # home manager, foydalanuvchi sozlamari boshqarish uchun ishlatiladi
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      # inputsdagi `follows` kaliti moslab olish uchun ishlatiladi.
+      # home manager ning `inputs.nixpkgs` si ushbu fleykning `inputs.nixpkgs`
+      # si bilan bir xil tarzda olib boriladi har xil bo'lgandagi muammolarni
+      # bartaraf etish uchun.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+ 
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      # ESLATMA iltimos hostname `my-nixos` ni o'zingiznikiga moslang
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          
+          # home manager ni nixos moduli qilib yuklang
+          # shunda home manager sozlamalari avtomatik tarzda qo'shilib ketadi `nixos-rebuild` ishga tushurilganda
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            
+            # ESLATMA ryan ni o'zingizni foydalanuvchi otingizga almashtiring
+            home-manager.users.muhammad = import ./home.nix;
+            
+            # Qo'shimchasiga home-manager.extraSpecialArgs ni home.nix ga argument oshirish uchun ishlatsangiz bo'ladi
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,90 @@
+{ config, pkgs, ... }:
+ 
+{
+  # Iltimos, foydalanuvchi nomini va uy katalogini oʻz holatingizga moslang
+  home.username = "muhammad";
+  home.homeDirectory = "/home/muhammad";
+ 
+  # Joriy katalogdagi konfiguratsiya faylini koʻrsatilgan joyga bogʻlash
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+ 
+  # `./scripts` ichidagi barcha fayllarni `~/.config/i3/scripts` ga bogʻlash
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # rekursiv bogʻlash
+  #   executable = true;  # barcha fayllarni bajariladigan qilish
+  # };
+ 
+  # Fayl mazmunini toʻgʻridan-toʻgʻri nix konfiguratsiyasiga yozish
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+ 
+  # Foydalanuvchi profiliga oʻrnatilishi kerak boʻlgan paketlar
+  home.packages = with pkgs; [
+    # ushbu keltirilganlar men kunlik davomida ishlatadiganlarim
+    # o'zingiznikini qo'shish yoki keltirilganlardan olib tashashdan tortinmang
+    
+    neofetch
+    nnn # terminal fayl menejer
+    
+    # arxivlar
+    zip
+    xz
+    unzip
+    p7zip
+    vim
+  ];
+ 
+  # oddiy git sozlamari, o'zingizga moslang
+  programs.git = {
+    enable = true;
+    userName = "sud0pacman";
+    userEmail = "mukhammad.kammoliddin@gmail.com";
+  };
+ 
+  # alacritty - platformalarar-o, GPU orqali tezlatilgan buyruq satr emulyatori
+  programs.alacritty = {
+    enable = true;
+    # moslangan sozlamalar
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+        size = 12;
+        draw_bold_text_with_bright_colors = true;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+    };
+  };
+ 
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    # ESLATMA o'zingizni bashrc'ingizni qo'shib keting
+    bashrcExtra = ''
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+    '';
+    
+    # qiqartma nomlar qo'shing, xoh qo'shing, xoh olib tashlang
+    shellAliases = {
+      k = "kubectl";
+      urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
+      urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
+      rebuild =  "sudo nixos-rebuild switch --flake /home/muhammad/workplace/sud0pacman/nix-config";
+    };
+  };
+ 
+  # Bu qiymat qaysi versiyadagi home-manager sozlamari bilan
+  # to'g'ri kelishini belgilaydi. Bu ko'proq home manager ning
+  # eski versiya bilan chiqishmaydigan yangi versiyasi chiqganda
+  # buzilib qolishlar oldini oladi.
+  # 
+  # Shu qiymat o'zgartirmayam home manager yangilasa bo'ladi. Ba'tafsil
+  # har bir relizda bo'lgan o'zgarishlar home managerning relizlar 
+  # eslatmasida ko'zdan kechirib chiqing.
+  home.stateVersion = "24.11";
+ 
+  # Home Manager o'zini o'rnatishiga qo'yib beraylik
+  programs.home-manager.enable = true;
+}
