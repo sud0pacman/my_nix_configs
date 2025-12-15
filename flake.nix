184bce692f6f48d51005a5e98d91c1eefa6c3707
bahrom04 <magdiyevbahrom@gmail.com>
2025-12-15 22:43:41 +0500

    chore: added shell.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "NixOS configuration";
- 
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     # home manager, foydalanuvchi sozlamari boshqarish uchun ishlatiladi
@@ -13,25 +13,29 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
- 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }:let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in  {
+    devShells."x86_64-linux".default = import ./shell.nix {inherit pkgs;};
+
     nixosConfigurations = {
       # ESLATMA iltimos hostname `my-nixos` ni o'zingiznikiga moslang
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
-          
+
           # home manager ni nixos moduli qilib yuklang
           # shunda home manager sozlamalari avtomatik tarzda qo'shilib ketadi `nixos-rebuild` ishga tushurilganda
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            
+
             # ESLATMA ryan ni o'zingizni foydalanuvchi otingizga almashtiring
             home-manager.users.muhammad = import ./home.nix;
-            
+
             # Qo'shimchasiga home-manager.extraSpecialArgs ni home.nix ga argument oshirish uchun ishlatsangiz bo'ladi
           }
         ];
---
+++ shell.nix
@@ -0,0 +1,20 @@
+{
+  pkgs,
+  ...
+}:
+  pkgs.stdenv.mkDerivation {
+    name = "my_nix_configs";
+
+    nativeBuildInputs = with pkgs; [
+      nixd
+      statix
+      deadnix
+      alejandra
+      age
+      sops
+      rng-tools
+      openssl
+    ];
+
+    NIX_CONFIG = "extra-experimental-features = nix-command flakes";
+  }
